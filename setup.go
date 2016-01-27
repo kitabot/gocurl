b63e627aa65873b3ffa16a379a460950200d6264
Andre Christoga <andrechristoga@gmail.com>
2016-01-28 06:53:47 +0700

    Better input

--- setup.go
+++ setup.go
@@ -17,15 +17,14 @@ func askURL() string {
 	if (url == "") ||
 		((!strings.HasPrefix(url, "http:")) ||
 			(!strings.HasPrefix(url, "http:"))) {
-		fmt.Println("Please type in a valid url")
+		fmt.Println("Please type in a valid url or use http://")
 		url = askURL()
 	}
 
 	return url
 }
 
-func main() {
-	fmt.Println(">> Insert url: (Make sure to use http://)")
+func main() {	
 	url := askURL()
 
 	data, err := http.Get(url)
